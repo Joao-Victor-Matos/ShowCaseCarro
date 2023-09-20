8b2ffcd56a4b6a0d264c305aff72b268ccb90955
JoaoMatos <queissotropa127@gmail.com>
2023-09-20 15:57:07 -0300

    correcao dos props da home

--- types/index.ts
+++ types/index.ts
@@ -38,6 +38,10 @@ export interface FilterProps {
   model: string;
 }
 
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
 export interface OptionsProps {
   title: string;
   value: string;
